ad747a54f3ea83c5f071cddc1644c181fc04a2b4
nenadstanic86 <nenadstanic86@gmail.com>
2023-08-30 13:18:20 +0200

    test

--- cypress/e2e/locateIngridBuilding.cy.js
+++ cypress/e2e/locateIngridBuilding.cy.js
@@ -1,7 +1,24 @@
 describe('template spec', () => {
   it('passes', () => {
+	  
+	cy.log('Open Google maps.')  
     cy.visit('https://www.google.com/maps')
-	cy.log('Google Maps are shown.')
+	cy.log('Google maps app is opened.')
+	cy.screenshot()
+	
+	cy.get('#searchboxinput').type('Schneider Electric LLC, Put partizanskog novosadskog odreda, Novi Sad')
+	cy.get('#searchbox-searchbutton').click()
+	cy.wait(2000)
+	cy.screenshot()
+	
+	cy.log('First zoom in level.') 
+	cy.get('#widget-zoom-in').click()
+	cy.wait(2000)
+	cy.screenshot()
+	
+	cy.log('Second zoom level.') 
+	cy.get('#widget-zoom-in').click()
+	cy.wait(2000)
 	cy.screenshot()
   })
 })
--- cypress/e2e/locateIngridBuilding2.cy.js
+++
@@ -1,7 +0,0 @@
-describe('template spec', () => {
-  it('passes', () => {
-    cy.visit('https://www.b92.net/')
-	cy.log('B92 site is opened.')
-	cy.screenshot()
-  })
-})
